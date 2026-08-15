2f426d842f7bff69038ecfeb22de8a419770a741
srihari <nalisrihari@gmail.com>
2026-08-15 17:56:30 +0530

    new director

---
+++ main.tf
@@ -0,0 +1,17 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = var.aws_region
+}
+
+resource "aws_s3_bucket" "jenkins_bucket" {
+  bucket = var.bucket_name
+
+}
